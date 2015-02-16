a516ff60a517898df24380b3096dc7a028710909
wayakorn <wayakorn@gmail.com>
2015-02-15 21:36:13 -0800

    AJAnywhere now uses modified base64 encoding to transmit data

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var aj = require('./routes/aj');
-var aj1 = require('./routes/aj');
-var aj2 = require('./routes/aj');
 
 var app = express();
 
@@ -27,8 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/aj', aj);
-app.use('/aj1', aj);
-app.use('/aj2', aj);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/aj.js
+++ routes/aj.js
@@ -30,7 +30,7 @@ router.get('/list/?:fromId([0-9]+)?', function(req, res) {
     res.end(body + '\n\n');
 });
 
-// Add operation
+// Add operation (GET)
 router.get('/add/:data', function(req, res) {
     var item = req.params.data;
     if (item) {
@@ -39,6 +39,8 @@ router.get('/add/:data', function(req, res) {
     }
     returnId(req, res);
 });
+
+// Add operation (POST)
 router.post('/add', function(req, res) {
     if (req.body) {
 		console.log(req.headers);
@@ -66,5 +68,103 @@ function returnId(req, res) {
     res.setHeader('cache-control', 'no-cache');
     res.end(body + '\n\n');
 }
+
+
+//
+// Schema functions
+//
+
+// Current schema version
+var g_currentSVersion = 0;
+
+// This is our dictionary object; we'll store items as properties, 
+// with "s_keys" property being the master list of all keys, which
+// is useful for enumerating items.
+var g_schemas = {}; 
+g_schemas['s_keys'] = [];
+
+// Schema update operation (GET)
+router.get('/s_updateget/:key/:value', function(req, res) {
+    var key = req.params.key;
+	var value = req.params.value;
+	console.log("k=" + key);
+	console.log("v=" + value);
+    if (key && value) {
+		var keys = g_schemas['s_keys'];
+		if (keys.length == 0 || g_schemas[key] != value) {
+			if (keys.indexOf(key) == -1) {
+				keys.push(key);
+				console.log("added k/v, total=" + keys.length.toString());
+			} else {
+				console.log("value was " + g_schemas[key]);
+				console.log("updating value, total=" + keys.length.toString());
+			}
+			g_schemas[key] = value;
+			g_currentSVersion++;
+		}
+    }
+    returnSVersion(req, res);
+});
+
+// Schema update operation (POST)  was '/s_update/:key'
+router.post(/\/s_updatepost\/(.+)/, function(req, res) {
+    if (req.body) {
+		console.log(req.headers);
+	}
+    var key = req.params[0];
+	var value = req.body;
+	console.log("k=" + key);
+	console.log("v=" + value);
+	if (key && value) {
+		var keys = g_schemas['s_keys'];
+		if (keys.length == 0 || g_schemas[key] != value) {
+			if (keys.indexOf(key) == -1) {
+				keys.push(key);
+				console.log("added k/v, total=" + keys.length.toString());
+			} else {
+				console.log("value was " + g_schemas[key]);
+				console.log("updating value, total=" + keys.length.toString());
+			}
+			g_schemas[key] = value;
+			g_currentSVersion++;
+		}
+	}
+    returnSVersion(req, res);
+});
+
+// Schema list operation
+router.get('/s_list', function(req, res) {
+    var body = "";
+	var keys = g_schemas['s_keys'];
+	console.log("count=" + keys.length);
+    for (var i = 0; i < keys.length; i++) {
+		body += "k=" + keys[i];
+		body += "\n";
+		body += "v=" + g_schemas[keys[i].toString()];
+		body += "\n\n";
+    }
+
+    res.setHeader('content-type', 'text/plain; charset=utf-8');
+    res.setHeader('cache-control', 'no-cache');
+    res.end(body + '\n\n');
+});
+
+// Schema removeall operation
+router.get('/s_removeall', function(req, res) {
+    g_currentSVersion++;
+	g_schemas['s_keys'] = [];
+    returnSVersion(req, res);
+});
+
+// Schema get ID operation
+router.get('/s_id', returnSVersion)
+
+function returnSVersion(req, res) {
+    var body = g_currentSVersion.toString();
+    res.setHeader('content-type', 'text/plain; charset=utf-8');
+    res.setHeader('cache-control', 'no-cache');
+    res.end(body + '\n\n');
+}
+
 
 module.exports = router;
