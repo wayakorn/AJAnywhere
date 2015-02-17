0aa0bb7cd7845d372e997b2422741e8f5e975dee
wayakorn <wayakorn@gmail.com>
2015-02-16 16:22:13 -0800

    AJAnywhere now supports multi-user.

--- routes/aj.js
+++ routes/aj.js
@@ -6,68 +6,103 @@ router.get('/', function(req, res) {
     res.render('aj_usage', { title: 'Usage' });
 });
 
-var g_currentId = 0;
+
+function returnNumber(req, res, id) {
+    var body = id.toString();
+    res.setHeader('content-type', 'text/plain; charset=utf-8');
+    res.setHeader('cache-control', 'no-cache');
+    res.end(body + '\n\n');
+}
+
+
+var g_currentId = [];
 var g_items = [];
 
 // List operation
-router.get('/list/?:fromId([0-9]+)?', function(req, res) {
-    var fromId = 0;
-    if (req.params.fromId)
-        fromId = req.params.fromId;
-
+router.get('/:user//list/?:fromId([0-9]+)?', function(req, res) {
     var body = "";
-    for (var i = 0; i < g_items.length; i++) {
-        if (g_items[i].id >= fromId) {
-            body += g_items[i].id.toString();
-            body += "=";
-            body += g_items[i].data;
-            body += "\n";
+    if (req.params.user) {
+        var user = req.params.user;
+        var fromId = 0;
+        if (req.params.fromId)
+            fromId = req.params.fromId;
+        if (!g_items[user])
+            g_items[user] = [];
+        for (var i = 0; i < g_items[user].length; i++) {
+            if (g_items[user][i].id >= fromId) {
+                body += g_items[user][i].id.toString();
+                body += "=";
+                body += g_items[user][i].data;
+                body += "\n";
+            }
         }
     }
-
     res.setHeader('content-type', 'text/plain; charset=utf-8');
     res.setHeader('cache-control', 'no-cache');
     res.end(body + '\n\n');
 });
 
 // Add operation (GET)
-router.get('/add/:data', function(req, res) {
+router.get('/:user//add/:data', function(req, res) {
+    var user = req.params.user;
     var item = req.params.data;
-    if (item) {
-        g_currentId++;
-        g_items.push({id:g_currentId, data:item});
-    }
-    returnId(req, res);
+    addCommon(req, res, user, item);
 });
 
 // Add operation (POST)
-router.post('/add', function(req, res) {
+router.post('/:user//add', function(req, res) {
+    var user = req.params.user;
+    var item = null;
     if (req.body) {
-		console.log(req.headers);
-		console.log(req.body);
-		var item = req.body.toString();
-        g_currentId++;
-        g_items.push({id:g_currentId, data:item});
+        console.log(req.headers);
+        console.log(req.body);
+        item = req.body.toString();
     }
-    returnId(req, res);
+    addCommon(req, res, user, item);
 });
 
+function addCommon(req, res, user, item) {
+    console.log("user=" + user);
+    console.log("item=" + item);
+
+    if (user && item) {
+        if (!g_currentId[user])
+            g_currentId[user] = 0;
+        g_currentId[user]++;
+        if (!g_items[user])
+            g_items[user] = [];
+        g_items[user].push({id:g_currentId[user], data:item});
+        if (!g_currentId[user])
+            g_currentId[user] = 0;
+        returnNumber(req, res, g_currentId[user]);
+    } else {
+        returnNumber(req, res, 0);
+    }
+}
+
 // RemoveAll operation
-router.get('/removeall', function(req, res) {
-    g_currentId = 0;
-    g_items = [];
-    returnId(req, res);
+router.get('/:user//removeall', function(req, res) {
+    if (!req.params.user) {
+        returnNumber(req, res, 0);
+    } else {
+        var user = req.params.user;
+        g_currentId[user] = 0;
+        g_items[user] = [];
+        returnNumber(req, res, g_currentId[user]);
+    }
 });
 
 // ID operation
-router.get('/id', returnId);
-
-function returnId(req, res) {
-    var body = g_currentId.toString();
-    res.setHeader('content-type', 'text/plain; charset=utf-8');
-    res.setHeader('cache-control', 'no-cache');
-    res.end(body + '\n\n');
-}
+router.get('/:user//id', function(req, res) {
+    if (!req.params.user) {
+        returnNumber(req, res, 0);
+    } else {
+        var user = req.params.user;
+        if (!g_currentId[user])
+            g_currentId[user] = 0;
+        returnNumber(req, res, g_currentId[user]);
+    }
+});
 
 
 //
@@ -75,96 +110,117 @@ function returnId(req, res) {
 //
 
 // Current schema version
-var g_currentSVersion = 0;
+var g_currentSchemaVersion = [];
 
-// This is our dictionary object; we'll store items as properties, 
+// This is an array of dictionary objects; we'll store items as properties, 
 // with "s_keys" property being the master list of all keys, which
 // is useful for enumerating items.
-var g_schemas = {}; 
-g_schemas['s_keys'] = [];
+var g_schemas = [];
 
 // Schema update operation (GET)
-router.get('/s_updateget/:key/:value', function(req, res) {
+router.get('/:user//s_updateget/:key/:value', function(req, res) {
+    var user = req.params.user;
     var key = req.params.key;
-	var value = req.params.value;
-	console.log("k=" + key);
-	console.log("v=" + value);
-    if (key && value) {
-		var keys = g_schemas['s_keys'];
-		if (keys.length == 0 || g_schemas[key] != value) {
-			if (keys.indexOf(key) == -1) {
-				keys.push(key);
-				console.log("added k/v, total=" + keys.length.toString());
-			} else {
-				console.log("value was " + g_schemas[key]);
-				console.log("updating value, total=" + keys.length.toString());
-			}
-			g_schemas[key] = value;
-			g_currentSVersion++;
-		}
-    }
-    returnSVersion(req, res);
+    var value = req.params.value;
+    s_updateCommon(req, res, user, key, value);
 });
 
 // Schema update operation (POST)  was '/s_update/:key'
-router.post(/\/s_updatepost\/(.+)/, function(req, res) {
+router.post(/\/(.+)\/\/s_updatepost\/(.+)/, function(req, res) {
     if (req.body) {
-		console.log(req.headers);
-	}
-    var key = req.params[0];
-	var value = req.body;
-	console.log("k=" + key);
-	console.log("v=" + value);
-	if (key && value) {
-		var keys = g_schemas['s_keys'];
-		if (keys.length == 0 || g_schemas[key] != value) {
-			if (keys.indexOf(key) == -1) {
-				keys.push(key);
-				console.log("added k/v, total=" + keys.length.toString());
-			} else {
-				console.log("value was " + g_schemas[key]);
-				console.log("updating value, total=" + keys.length.toString());
-			}
-			g_schemas[key] = value;
-			g_currentSVersion++;
-		}
-	}
-    returnSVersion(req, res);
+        console.log(req.headers);
+    }
+    var user = req.params[0];
+    var key = req.params[1];
+    var value = req.body;
+    s_updateCommon(req, res, user, key, value);
 });
 
-// Schema list operation
-router.get('/s_list', function(req, res) {
-    var body = "";
-	var keys = g_schemas['s_keys'];
-	console.log("count=" + keys.length);
-    for (var i = 0; i < keys.length; i++) {
-		body += "k=" + keys[i];
-		body += "\n";
-		body += "v=" + g_schemas[keys[i].toString()];
-		body += "\n\n";
+function s_updateCommon(req, res, user, key, value) {
+    console.log("user=" + user);
+    console.log("k=" + key);
+    console.log("v=" + value);
+    
+    if (user && key && value) {
+        if (!g_schemas[user]) {
+            g_schemas[user] = {};
+            g_schemas[user]['s_keys'] = [];
+        }
+        var keys = g_schemas[user]['s_keys'];
+        if (keys.length == 0 || g_schemas[user][key] != value) {
+            if (keys.indexOf(key) == -1) {
+                keys.push(key);
+                console.log("added k/v, total=" + keys.length.toString());
+            } else {
+                console.log("value was " + g_schemas[user][key]);
+                console.log("updating value, total=" + keys.length.toString());
+            }
+            g_schemas[user][key] = value;
+            if (!g_currentSchemaVersion[user])
+                g_currentSchemaVersion[user] = 0;
+            g_currentSchemaVersion[user]++;
+        }
+        if (!g_currentSchemaVersion[user])
+            g_currentSchemaVersion[user] = 0;
+        returnNumber(req, res, g_currentSchemaVersion[user]);
+    } else {
+        returnNumber(req, res, 0);
     }
+}
 
+// Schema list operation
+router.get('/:user//s_list', function(req, res) {
+    var body = "";
+    if (!req.params.user) {
+        returnNumber(req, res, 0);
+    } else {
+        var user = req.params.user;
+        if (!g_schemas[user]) {
+            g_schemas[user] = {};
+            g_schemas[user]['s_keys'] = [];
+        }
+        var keys = g_schemas[user]['s_keys'];
+        console.log("count=" + keys.length);
+        for (var i = 0; i < keys.length; i++) {
+            body += "k=" + keys[i];
+            body += "\n";
+            body += "v=" + g_schemas[user][keys[i].toString()];
+            body += "\n\n";
+        }
+    }
     res.setHeader('content-type', 'text/plain; charset=utf-8');
     res.setHeader('cache-control', 'no-cache');
     res.end(body + '\n\n');
 });
 
 // Schema removeall operation
-router.get('/s_removeall', function(req, res) {
-    g_currentSVersion++;
-	g_schemas['s_keys'] = [];
-    returnSVersion(req, res);
+router.get('/:user//s_removeall', function(req, res) {
+    if (!req.params.user) {
+        returnNumber(req, res, 0);
+    } else {
+        var user = req.params.user;
+        if (!g_currentSchemaVersion[user])
+            g_currentSchemaVersion[user] = 0;
+        g_currentSchemaVersion[user]++;
+        if (!g_schemas[user]) {
+            g_schemas[user] = {};
+        }
+        g_schemas[user]['s_keys'] = [];
+        returnNumber(req, res, g_currentSchemaVersion[user]);
+    }
 });
 
 // Schema get ID operation
-router.get('/s_id', returnSVersion)
-
-function returnSVersion(req, res) {
-    var body = g_currentSVersion.toString();
-    res.setHeader('content-type', 'text/plain; charset=utf-8');
-    res.setHeader('cache-control', 'no-cache');
-    res.end(body + '\n\n');
-}
+router.get('/:user//s_id', function(req, res) {
+    if (!req.params.user) {
+        returnNumber(req, res, 0);
+    } else {
+        var user = req.params.user;
+        if (!g_currentSchemaVersion[user])
+            g_currentSchemaVersion[user] = 0;
+        returnNumber(req, res, g_currentSchemaVersion[user]);
+    }
+});
 
 
 module.exports = router;
