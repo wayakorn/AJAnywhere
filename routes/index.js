6720fe846ca061035e3ad73a363caea77ac57819
Way Vadhanasin <wayvad@microsoft.com>
2017-01-25 15:53:33 -0800

    Add PlayTimeIsOver.js route.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'index.js' });
 });
 
+router.get('/PlayTimeIsOver', function(req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end('{\n\"uid\": \"urn:uuid:feedbabe-feed-babe-food-foodfood0001\",\n\"updateDate\": "2017-01-25T00:00:00.0Z\",\n\"titleText\": \"Play Time Is Over Feed\",\n\"mainText": \"Play time is over, boys.\"\n}');
+});
+
 module.exports = router;
